c72a6d48cf00c4c1ae7e4deeadf684f1b2ae86fa
Yanisa Thaenthong <mook.yanisa@gmail.com>
2024-08-09 18:25:06 +0700

    Create RegisterView

---
+++ src/views/RegisterView.vue
@@ -0,0 +1,86 @@
+<template>
+    <UserLayout>
+        <div class="h-screen ">
+            <div class="flex flex-col justify-center items-center">
+                <div class="bg-white rounded-3xl border-y-indigo-950 shadow-lg mt-10 p-12 md:w-2/5">
+                    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+                        <h2 class="text-center text-3xl font-bold leading-9 tracking-tight text-gray-900">
+                            Register
+                        </h2>
+                    </div>
+
+                    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                        <form @submit="handlesubmit" class="space-y-7">
+                            <div>
+                                <label class="input input-bordered flex items-center gap-2">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
+                                        class="h-4 w-4 opacity-70">
+                                        <path
+                                            d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+                                        <path
+                                            d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+                                    </svg>
+                                    <input id="email" type="text" class="grow" placeholder="Email" />
+                                </label>
+                            </div>
+
+                            <div>
+                                <label class="input input-bordered flex items-center gap-2">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
+                                        class="h-4 w-4 opacity-70">
+                                        <path
+                                            d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+                                    </svg>
+                                    <input id="username" type="text" class="grow" placeholder="Username" />
+                                </label>
+                            </div>
+
+                            <div>
+                                <label class="input input-bordered flex items-center gap-2">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
+                                        class="h-4 w-4 opacity-70">
+                                        <path fill-rule="evenodd"
+                                            d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                                            clip-rule="evenodd" />
+                                    </svg>
+                                    <input id="password" type="password" class="grow" placeholder="Password" />
+                                </label>
+                            </div>
+
+                            <div>
+                                <button class="btn btn-neutral flex w-full justify-center rounded-md">Register</button>
+                            </div>
+                        </form>
+
+                        <p class="mt-10 text-center text-sm text-gray-500">
+                            Already have an account?
+                            <RouterLink to="/login" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Login</RouterLink>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </UserLayout>
+</template>
+
+<script setup>
+import UserLayout from '@/layouts/UserLayout.vue';
+
+const handlesubmit = (e) => {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    console.log(email, username, password);
+    localStorage.setItem('email', email);
+    localStorage.setItem('username', username);
+    localStorage.setItem('password', password);
+    
+    if (localStorage.getItem('email') && localStorage.getItem('username') && localStorage.getItem('password')) {
+        window.location.href = '/login';
+    }
+}
+
+</script>
+
+<style lang="scss" scoped></style>
