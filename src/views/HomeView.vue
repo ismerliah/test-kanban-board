476eaa0e8673b50e716a9f199884a7bf42aa9283
Yanisa Thaenthong <mook.yanisa@gmail.com>
2024-08-09 18:24:11 +0700

    Create StartView and HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,11 @@
 <template>
-  Hello World
+  <UserLayout>
+    Hello World
+  </UserLayout>
 </template>
 <script setup>
+import UserLayout from '@/layouts/UserLayout.vue';
+
 
 </script>
 
---
+++ src/views/StartView.vue
@@ -0,0 +1,22 @@
+<template>
+    <UserLayout>
+      <div class="hero bg-base-200 min-h-screen">
+        <div class="hero-content text-center">
+          <div class="max-w-md">
+            <h1 class="text-5xl font-bold">Kanban Board</h1>
+            <p class="py-6">
+              Managing homework can be overwhelming, especially when you have multiple assignments with different deadlines.
+            </p>
+            <RouterLink to="/login" class="btn btn-primary">Get Started</RouterLink>
+          </div>
+        </div>
+      </div>
+    </UserLayout>
+  </template>
+  <script setup>
+  import UserLayout from '@/layouts/UserLayout.vue';
+  
+  
+  </script>
+  
+  <style lang="scss" scoped></style>
